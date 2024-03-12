e74c6f353edca388381f7b55e1a7ecaf26b15710
meysam-kazemi <meisam.daysu@gmail.com>
2024-03-12 18:50:51 +0330

    set configuration variables

--- classifier.py
+++ classifier.py
@@ -7,3 +7,15 @@ import torchvision
 from torchvision import transforms
 import matplotlib.pyplot as plt
 
+# Configuration variables
+TRAIN_DATA_PATH = "./cards/train/"
+TEST_DATA_PATH = "./cards/test/"
+VALID_DATA_PATH = "./cards/valid/"
+BATCH_SIZE = 64
+
+TRANSFORM_IMG = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225] )
+])
+
